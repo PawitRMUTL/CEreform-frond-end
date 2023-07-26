f82bef91fca872f9b8f609519c02c36e2fb7d938
pawit pitukthong <97580507+PawitRMUTL@users.noreply.github.com>
2023-07-27 01:30:17 +0700

    update teacher

--- app/containers/Pages/Personal/index.js
+++ app/containers/Pages/Personal/index.js
@@ -1,56 +1,88 @@
 /** @format */
 
-import React from 'react';
-import { Box, Card } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Box, Card, Backdrop } from '@mui/material';
+import axios from 'axios';
 import Button from '@mui/material/Button';
-import { makeStyles } from 'tss-react/mui';
+// import Fade from '@mui/material/Fade';
+
 import Modal from '@mui/material/Modal';
+import email from 'dan-images/email.png';
+import telephone from 'dan-images/telephone.png';
+import { GrClose } from 'react-icons/Gr';
 import './styles.css';
 
-function getModalStyle() {
-  const top = 50;
-  const left = 50;
-
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  };
-}
-
-const useStyles = makeStyles()((theme) => ({
-  paper: {
-    position: 'absolute',
-    width: 400,
-    backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
-}));
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 480,
+  height: 800,
+  borderRadius: '50px',
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  p: 4,
+  fontFamily: 'Noto Sans Thai',
+};
 
 function personal() {
-  const { classes } = useStyles();
-  // getModalStyle is not a pure function, we roll the style only on the first render
-  const [modalStyle] = React.useState(getModalStyle);
-  const [open, setOpen] = React.useState(false);
-
-  const handleOpen = () => {
+  const [teacherData, Setteacherdata] = useState([]);
+  const [imageDATA, setImageDATA] = useState([]);
+  const [checkvalue, Setcheckvalue] = useState(false);
+  const [tumbdata, Settumbdata] = useState([]);
+  const [tumbid, Settumbid] = useState(0);
+  const [tumbteacher, Settumbteacher] = useState([]);
+  // Modal Zone
+  const [open, setOpen] = useState(false);
+  const handleOpen = (imagevalues, id) => {
+    Settumbdata(imagevalues);
+    Settumbid(id);
     setOpen(true);
   };
+  const handleClose = () => setOpen(false);
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+  useEffect(() => {
+    if (tumbid !== undefined) {
+      axios
+        .post('http://0.0.0.0:3200/api/ReadTeacherByID', { id: tumbid })
+        .then((response) => Settumbteacher(response.data))
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [tumbid]);
 
-  const body = (
-    <div style={modalStyle} className={classes.paper}>
-      <h2 id='simple-modal-title'>Text in a modal</h2>
-      <p id='simple-modal-description'>
-        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-      </p>
-    </div>
-  );
+  // // // // // // // // // // // // // // // //
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          'http://0.0.0.0:3200/api/Teacher_list'
+        );
+        if (!checkvalue) {
+          Setcheckvalue(true);
+          Setteacherdata(response.data);
+        }
+      } catch (error) {
+        console.log('error fetchData is ', error);
+      }
+    };
+
+    fetchData();
+  }, [checkvalue]);
+  useEffect(() => {
+    if (teacherData !== undefined) {
+      // let ImageValue;
+      const promises = Object.values(teacherData).map((data) => import(`/Users/baconinhell/Desktop/dandelion-pro_v25/starter-project/image/teacher/${data._image}`).then((image) => image.default));
+      Promise.all(promises).then((imagePaths) => {
+        const ImageValue = [];
+        imagePaths.forEach((index) => ImageValue.push(index));
+        setImageDATA(ImageValue);
+      });
+    }
+  }, [teacherData]);
 
   return (
     <>
@@ -72,91 +104,128 @@ function personal() {
 
       <section className='section'>
         <article>
-          {Array.from(Array(16)).map((_, index) => (
-            <Card className='Card' key={index}w>
-              <img src='/images/avatars/pp_boy2.svg' />
-              <h2>John Dose </h2>
-              <h4>
-                จอน เดดด <br />
-                Johndo@gmail.com
-              </h4>
-              <div>
-                <Button
-                  type='button'
-                  onClick={handleOpen}
-                  sx={{
-                    color: '#FE6F41',
-                    background:
-                      'linear-gradient(white, white) padding-box, linear-gradient(to right, #FE6F41, #F8BA1C) border-box',
-                    borderRadius: '50em',
-                    border: '1px solid transparent',
-                    '&:hover': {
-                      background: 'linear-gradient(#FE6F41, #F8BA1C)',
-                      color: '#FFF',
-                      border: '1px solid',
-                    },
-                  }}>
-                  อ่านเพิ่มเติม
-                </Button>
-                <Modal
-                  open={open}
-                  onClose={handleClose}
-                  aria-labelledby='simple-modal-title'
-                  aria-describedby='simple-modal-description'>
-                  {body}
-                </Modal>
-              </div>
-            </Card>
+          {Object.values(teacherData).map((data, index) => (
+            <div key={data.teacher_id}>
+              <Card className='Card'>
+                <img src={imageDATA[index]} />
+                <h2>John Dose </h2>
+                <h4>
+                  {data.prefix} {data.first_name} {data.last_name}
+                  <br />
+                  {data._email}
+                </h4>
+                <div>
+                  <Button
+                    type='button'
+                    onClick={() => handleOpen(imageDATA[index], data.teacher_id)}
+                    sx={{
+                      color: '#FE6F41',
+                      background:
+                        'linear-gradient(white, white) padding-box, linear-gradient(to right, #FE6F41, #F8BA1C) border-box',
+                      borderRadius: '50em',
+                      border: '1px solid transparent',
+                      '&:hover': {
+                        background: 'linear-gradient(#FE6F41, #F8BA1C)',
+                        color: '#FFF',
+                        border: '1px solid',
+                      },
+                    }}>
+                    อ่านเพิ่มเติม
+                  </Button>
+                </div>
+              </Card>
+            </div>
           ))}
+          <Modal
+            aria-labelledby='modal-modal-title'
+            aria-describedby='modal-modal-description'
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            slots={{ backdrop: Backdrop }}
+            slotProps={{
+              backdrop: {
+                timeout: 900,
+              },
+            }}>
+            <div>
+              {Object.values(tumbteacher).map((data) => (
+                <div key={data.teacher_id}>
+                  <Box sx={style} className='ModalLayout'>
+                    <div className='ModalCloseIcon' onClick={handleClose} >
+                      <GrClose />
+                    </div>
+                    <img
+                      src={tumbdata}
+                      style={{
+                        boxShadow: ' 0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
+                      }}
+                    />
+                    <span className='Modaltitle' style={{ marginTop: '2rem' }}>
+                      {data.prefix} {data.first_name} {data.last_name}
+                    </span>
+                    <span className='Modaltitle'>lerm lopadle</span>
+                    <span className='ModalEmail'>{data._email}</span>
+                    <div className='ModalSubject'>
+                      <span>คณะ {data.subject} </span>
+                      <span>สาขาวิชา {data.major}</span>
+                    </div>
+                    <span
+                      style={{
+                        color: '#000',
+                        textAlign: 'left',
+                        fontSize: '18px',
+                        fontWeight: 700,
+                        width: '100%',
+                        marginBottom: '1%',
+                      }}>
+                      คุณวุฒิ
+                    </span>
+                    <div className='ModalEducation'>
+                      <span>ป.ตรี</span>
+                      <span>univercity</span>
+                      <span>ป.โท</span>
+                      <span>univercity</span>
+                    </div>
+                    <span
+                      style={{
+                        color: '#000',
+                        textAlign: 'left',
+                        fontSize: '18px',
+                        fontWeight: 700,
+                        width: '100%',
+                        marginBottom: '1%',
+                      }}>
+                      รายวิชาที่รับผิดชอบ
+                    </span>
+                    <div className='ModalSujectForTeccher'>
+                      <ui>
+                        <li>---</li>
+                        <li>---</li>
+                        <li>---</li>
+                        <li>---</li>
+                        <li>---</li>
+                        <li>---</li>
+                      </ui>
+                    </div>
+                    <div className='Modalicon'>
+                      <div>
+                        <img src={email} />
+                        <span>{data._email}</span>
+                      </div>
+                      <div>
+                        <img src={telephone} />
+                        <span>{data._phone}</span>
+                      </div>
+                    </div>
+                  </Box>
+                </div>
+              ))}
+            </div>
+          </Modal>
         </article>
       </section>
     </>
   );
 }
 export default personal;
-
-// {
-//   <Grid
-//               container
-//               spacing={{ xs: 2, md: 6 }}
-//               columns={{ xs: 2, sm: 8, md: 12 }}>
-//               {Array.from(Array(16)).map((_, index) => (
-//                 <Grid item xs={2} sm={4} md={4} key={index}>
-//                   <Card className='Card'>
-//                     <img src='/images/avatars/pp_boy2.svg' />
-//                     <h2>John Dose </h2>
-//                     <h4>
-//                       จอน เดดด <br />
-//                       Johndo@gmail.com
-//                     </h4>
-//                     <div>
-//                       <Button
-//                         type='button'
-//                         onClick={handleOpen}
-//                         sx={{
-//                           color: '#FE6F41',
-//                           background:
-//                             'linear-gradient(white, white) padding-box, linear-gradient(to right, #FE6F41, #F8BA1C) border-box',
-//                           borderRadius: '50em',
-//                           border: '1px solid transparent',
-//                           '&:hover': {
-//                             background: 'linear-gradient(#FE6F41, #F8BA1C)',
-//                             color: '#FFF',
-//                             border: '1px solid',
-//                           },
-//                         }}>
-//                         อ่านเพิ่มเติม
-//                       </Button>
-//                       <Modal
-//                         open={open}
-//                         onClose={handleClose}
-//                         aria-labelledby='simple-modal-title'
-//                         aria-describedby='simple-modal-description'>
-//                         {body}
-//                       </Modal>
-//                     </div>
-//                   </Card>
-//                 </Grid>
-//               ))}
-//             </Grid>
-// }
