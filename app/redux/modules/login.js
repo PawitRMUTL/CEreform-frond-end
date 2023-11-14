e2db874b2bae61efc4a798726c0a5d5eb5100e41
pawit pitukthong <97580507+PawitRMUTL@users.noreply.github.com>
2023-11-14 09:23:48 +0700

    Chage Redux

--- app/redux/modules/login.js
+++ app/redux/modules/login.js
@@ -7,23 +7,23 @@ const initialState = {
     // password: '0000-00-00',
     // username: '60523206001-8',
     // password: '20200701',
-    username: 'pichit@rmutl.ac.th',
+    // username: 'pichit@rmutl.ac.th',
     // username: 'kwanchai@rmutl.ac.th',
     // username: 'john@rmutl.ac.th',
-    password: '10101',
-    remember: false
-  }
+    // password: '10101',
+    remember: false,
+  },
 };
 
 /* eslint-disable default-case, no-param-reassign */
-const loginReducer = (state = initialState, action = {}) => produce(state, draft => {
-  switch (action.type) {
-    case INIT:
-      draft.userLogin = state;
-      break;
-    default:
-      break;
-  }
-});
+const loginReducer = (state = initialState, action = {}) => produce(state, (draft) => {
+    switch (action.type) {
+      case INIT:
+        draft.usersLogin = state;
+        break;
+      default:
+        break;
+    }
+  });
 
 export default loginReducer;
--- dockerfile
+++ dockerfile
@@ -4,7 +4,6 @@ WORKDIR /app
 COPY ./ /app/
 RUN npm i
 RUN npm run build
-# RUN npm run build:dll
 # Stage 1, based on Nginx, to have only the compiled app, ready for production with Nginx
 FROM nginx:latest
 COPY --from=build-stage /app/build/ /usr/share/nginx/html
